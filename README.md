d4b37b442a9568cf1d570a2281b9949233c117d5
Xavier Perseguers <xavier@causal.ch>
2023-10-04 17:28:48 +0200

    [TASK] Add skeleton files for a TYPO3 extension

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# t3ext-mfa-protect
-Protect rendering of sensitive content elements with MFA
+# MFA Protect
+
+This extension lets you force the use of a recent MFA before rendering a content
+element.
+
+This may be used together with standard access restrictions in TYPO3 such as
+being part of one or more Frontend user groups.
---
+++ ext_emconf.php
@@ -0,0 +1,27 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "mfa_protect".
+ *
+ * Auto generated 04-10-2023 16:41
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'MFA Protect',
+    'description' => 'Protect rendering of sensitive content elements with MFA.',
+    'category' => 'services',
+    'version' => '0.1-dev',
+    'state' => 'alpha',
+    'createDirs' => '',
+    'author' => 'Xavier Perseguers',
+    'author_email' => 'xavier@causal.ch',
+    'author_company' => 'Causal Sàrl',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.2.0-8.2.99',
+            'typo3' => '11.5.0-12.4.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
