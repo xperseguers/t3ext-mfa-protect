81513fb032fd444a130a2cb586cd33413848108f
Xavier Perseguers <xavier@causal.ch>
2023-10-05 18:30:19 +0200

    [TASK] Implement check on the validity of the MFA token

--- Classes/Controller/ContentController.php
+++ Classes/Controller/ContentController.php
@@ -18,13 +18,16 @@ namespace Causal\MfaProtect\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\TypoScript\TypoScriptService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 class ContentController extends ActionController
 {
     protected static int $instances = 0;
+    protected static int $tokenValidity = 0;
 
     protected ContentObjectRenderer $contentObjectRenderer;
 
@@ -48,20 +51,22 @@ class ContentController extends ActionController
 
     protected function isMfaTokenRecent(): bool
     {
-        // TODO: check on 'POST' and static::$instance === 0 when MFA check below is implemented
-        if ($this->request->getMethod() === 'POST') {
+        if ($this->request->getMethod() === 'POST' && static::$instances === 0) {
             $otp = $this->request->getParsedBody()['tx_mfaprotect_otp'] ?? '';
             if (preg_match('/^[0-9]{6}$/', $otp)) {
                 // TODO: check OTP and store as used recently
                 if ($otp === '123456') {
+                    $this->getFrontendUserAuthentication()->setSessionData('mfa_protect.time', $GLOBALS['EXEC_TIME']);
                     // TODO: shall we redirect instead in order to prevent serving from a POST request?
                     return true;
                 }
             }
         }
 
-        // TODO: check if MFA token is recent enough
-        return false;
+        $tokenValidity = $this->getTokenValidity();
+        $lastCheck = $this->getFrontendUserAuthentication()->getSessionData('mfa_protect.time') ?: 0;
+
+        return $lastCheck >= $tokenValidity;
     }
 
     protected function renderActualContent(): string
@@ -79,5 +84,33 @@ class ContentController extends ActionController
             ->fetchAssociative();
         $this->contentObjectRenderer->start($row, 'tt_content', $this->request);
         return $this->contentObjectRenderer->cObjGetSingle('< tt_content', []);
+    }
+
+    protected function getTokenValidity(): int
+    {
+        if (static::$tokenValidity > 0) {
+            return static::$tokenValidity;
+        }
+
+        $tsKey = 'tokenValidity';
+        $tokenValidity = $this->settings[$tsKey] ?? null;
+        if (is_array($tokenValidity)) {
+            // Dynamic value
+            $typoScriptService = GeneralUtility::makeInstance(TypoScriptService::class);
+            $settings = $typoScriptService->convertPlainArrayToTypoScriptArray($this->settings);
+            $tokenValidity = $this->contentObjectRenderer->cObjGetSingle(
+                $settings[$tsKey] ?? '',
+                $settings[$tsKey . '.'] ?? []
+            );
+        }
+
+        $tokenValidity = max(0, (int)$tokenValidity);
+        static::$tokenValidity = $GLOBALS['EXEC_TIME'] - 60 * $tokenValidity;
+        return static::$tokenValidity;
+    }
+
+    protected function getFrontendUserAuthentication(): FrontendUserAuthentication
+    {
+        return $this->request->getAttribute('frontend.user');
     }
 }
