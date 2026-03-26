134523cead866b694d61c47301241bb95294b7c1
Xavier Perseguers <xavier@causal.ch>
2026-03-26 09:51:17 +0100

    [TASK] Use hack to insert custom field at proper position in TCA
    Related: https://forge.typo3.org/issues/109375

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -15,9 +15,22 @@ $tempColumns = [
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-    'tt_content',
-    'tx_mfaprotect_enable',
-    '',
-    'before:starttime'
-);
+if ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() >= 13) {
+    // There is an issue with auto-created system TCA columns since TYPO3 v13.
+    // See https://forge.typo3.org/issues/109375
+    // As such, we hack the "hidden" palette to add our field to have a similar UX
+    // as in previous TYPO3 versions.
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
+        'tt_content',
+        'hidden',
+        '--linebreak--,tx_mfaprotect_enable',
+        'after:hidden'
+    );
+} else {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        'tx_mfaprotect_enable',
+        '',
+        'before:starttime'
+    );
+}
